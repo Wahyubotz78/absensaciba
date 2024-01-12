9f313ca4e6b53afbc3e6fde16d0133d365184206
Vyperou <wyzlure76@gmail.com>
2024-01-12 22:20:43 +0700

    pindahin proses uplod

--- halaman/profile.php
+++ halaman/profile.php
@@ -45,6 +45,45 @@ exit(); // Pastikan untuk menghentikan eksekusi skrip setelah pengalihan header
     }
   </style>
 
+<?php
+include '../koneksi.php';
+if(isset($_POST['submit']))
+   {
+   
+   $imgfile=$_FILES["fotomurid"]["name"];
+   // get the image extension
+   $extension = substr($imgfile,strlen($imgfile)-4,strlen($imgfile));
+   // allowed extensions
+   $allowed_extensions = array(".jpg",".jpeg",".png",".gif");
+   // Validation for allowed extensions .in_array() function searches an array for a specific value.
+   if(!in_array($extension,$allowed_extensions))
+   {
+   echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+   }
+   else
+   {
+   //rename the image file
+   $imgnewfile=md5($imgfile).$extension;
+   // Code for move image into directory
+   move_uploaded_file($_FILES["fotomurid"]["tmp_name"],"../fotomurid/".$imgnewfile);
+   
+   $query=mysqli_query($koneksi,"update datamurid set fotomurid='$imgnewfile' where nis='$_SESSION[nis]'");
+   if($query)
+   {
+    echo '<meta http-equiv="refresh" content="0;url=profile.php">';
+   }
+   else{
+   $error="Something went wrong . Please try again.";
+   echo '
+        <script>
+            alert("Maaf Foto anda terlalu berat");
+            window.location.href=profile.php
+        </script>';  
+   } 
+   }
+   }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- halaman/script.php
+++ halaman/script.php
@@ -41,39 +41,7 @@
     }
   </style>
 
-<?php
-include '../koneksi.php';
-if(isset($_POST['submit']))
-   {
-   
-   $imgfile=$_FILES["fotomurid"]["name"];
-   // get the image extension
-   $extension = substr($imgfile,strlen($imgfile)-4,strlen($imgfile));
-   // allowed extensions
-   $allowed_extensions = array(".jpg",".jpeg",".png",".gif");
-   // Validation for allowed extensions .in_array() function searches an array for a specific value.
-   if(!in_array($extension,$allowed_extensions))
-   {
-   echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
-   }
-   else
-   {
-   //rename the image file
-   $imgnewfile=md5($imgfile).$extension;
-   // Code for move image into directory
-   move_uploaded_file($_FILES["fotomurid"]["tmp_name"],"../fotomurid/".$imgnewfile);
-   
-   $query=mysqli_query($koneksi,"update datamurid set fotomurid='$imgnewfile' where nis='$_SESSION[nis]'");
-   if($query)
-   {
-    echo '<meta http-equiv="refresh" content="0;url=profile.php">';
-   }
-   else{
-   $error="Something went wrong . Please try again.";    
-   } 
-   }
-   }
-?>
+
       <!-- Ganti PP Modal-->
       <div class="modal fade" id="gantippModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
